13ac0a1e28092a68450a1c5c91ed15e1134ecff3
walletkun <walletkun@gmail.com>
2024-09-21 23:37:14 -0400

    Initialized Pinecone

--- app/layout.js
+++ app/layout.js
@@ -6,11 +6,14 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
 
 export const metadata = {
   title: "Create Next App",
