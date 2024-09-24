34cd3618a8f92d5b45009b4e24bee375371a4a11
walletkun <walletkun@gmail.com>
2024-09-23 20:31:59 -0400

    Rerouted the directories

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
 import { Manrope } from 'next/font/google'
-import { cn } from '@/lib/utils'
+import { cn } from '@/app/lib/utils'
 import './globals.css'
 
 const fontHeading = Manrope({
