8762578c84a2d46dee7bb86af753d83fde7bddae
walletkun <walletkun@gmail.com>
2024-09-24 17:01:05 -0400

    Added a bunch of components and still working on the registration/login

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("846e5c29e65f08a0")
+/******/ 		__webpack_require__.h = () => ("16ae836e268f5568")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
---
+++ components/auth/auth-header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AuthHeader = ({label, title}) => {
+  return (
+    <div className='w-full flex flex-col gap-y-4 items-center justify-center'>
+      <h1 className='text-3xl font-semibold'>{title}</h1>
+      <p className="text-muted-foreground text-sm">{label}</p>
+    </div>
+  )
+}
+
+export default AuthHeader
