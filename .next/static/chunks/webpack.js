5dd6f3a9a705f9357501965879f2c803d6dc5072
walletkun <walletkun@gmail.com>
2024-09-24 14:31:09 -0400

    added info

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "20b7726338b00e78"; }
+/******/ 		__webpack_require__.h = function() { return "bdb7a6516ff21be1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
