cca0c2990403f3159d6f7689624b0259ee18ac01
Mohammad Najm <128323124+Mnajm6201@users.noreply.github.com>
2024-09-29 08:55:40 -0400

    made pinecone

---
+++ app/chunks/chunk.py
@@ -0,0 +1,19 @@
+# Name: chunk.py
+# Description: Creates a class called Chunk which is filled with 
+# text and meta data of a chunk of a parsed txt 
+# to allow for upload to pinecone.
+class Chunk:
+    def __init__(self, text, annotations="", source_info="", tags=""):
+        """
+        Initializes a Chunk object with basic metadata.
+        
+        :param text: The main content of the chunk.
+        :param annotations: Annotations or notes related to the chunk (default is "").
+        :param source_info: Information about the source, like textbook name or chapter (default is "").
+        :param tags: A string of tags or keywords related to the chunk (default is "").
+        """
+        self.text = text
+        self.annotations = annotations
+        self.source_info = source_info
+        self.tags = tags
+
